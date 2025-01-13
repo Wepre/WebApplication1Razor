2ff6933556f40a5078c8d62f1f1cc0be0beb2118
FINNIAN X <1771555693@qq.com>
2025-01-13 18:14:18 +0800

    第一次提交

--- Model/LoginModel.cs
+++ Model/LoginModel.cs
@@ -2,6 +2,8 @@
 {
     public class LoginModel
     {
-        
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
